4a5444708cd3acd66ffb66a8e4a3b37018771ca4
Tech Madi <zolasmandela4@gmail.com>
2022-01-05 11:41:58 +0300

    Data Binding in Angular Forms

--- src/app/notes.ts
+++ src/app/notes.ts
@@ -2,4 +2,14 @@
  * Angular Forms Module
  * Bootstrap for Styling
  * Form controls : checkbox, text area , input etc etc
+ *
+ *
+ *
+ * Data binding in Angular Forms
+ * Ng form
+ *NgModel
+ * Data Model
+ * Two way data minding
+ * Copying Form Data
+ *
  */
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from './../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.scss']
+  styleUrls: ['./user-settings-form.component.scss'],
 })
 export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: IUserSettings = {
+    name: 'Madi',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'annual',
+    notes: 'Here are some notes',
+  };
 
-  constructor() { }
+  userSettings: IUserSettings = { ...this.originalUserSettings };
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
